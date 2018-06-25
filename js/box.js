0d100009b3fadef72c24f9cb26fdc19891c85424
James Gualtieri <jgualtieri2310@gmail.com>
2018-06-25 19:21:15 -0400

    idkkkk

--- js/box.js
+++ js/box.js
@@ -1,9 +1,9 @@
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, 4*window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera( 75 , window.innerWidth/window.innerHeight, 0.1, 1000 );
 var light = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, 0.2*window.innerHeight );
+renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var geometry = new THREE.BoxGeometry( 3, 3, 3 );
